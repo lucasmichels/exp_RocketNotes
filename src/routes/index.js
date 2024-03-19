7d8bab682ddb63a606c15df1f1bbdf9b51e524a8
lucas <lucass.michels@gmail.com>
2024-03-19 12:40:38 -0400

    Listando Tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 
 const userRouter = require("./user.routes");
 const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
 
 const routes = Router();
 routes.use("/users", userRouter); //1- acha as routes e vai para userRouter
 routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
 
 module.exports = routes;
